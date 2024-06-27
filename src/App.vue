14fd8c78e65f8346306b18efc5be64cffc42c284
rutkovskuy1 <roman.rutkovskyi@kpk-lp.com.ua>
2024-06-27 21:49:52 +0300

    components and main files
    This is where the main files and components of the site are located

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <HeaderComponent @navigate="navigate"/>
+    <component :is="currentComponent"></component>
+    <FooterComponent />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+import MainContent from './components/MainContent.vue';
+import CarCatalog from './components/CarCatalog.vue'; 
+import AboutUs from './components/AboutUs.vue'; 
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent,
+    FooterComponent,
+    MainContent,
+    CarCatalog ,
+    AboutUs
+  },
+  data() {
+    return {
+      currentComponent: 'MainContent'
+    };
+  },
+  methods: {
+    navigate(component) {
+      this.currentComponent = component;
+    }
+  }
+}
+</script>
+
---
+++ src/components/CarCatalog.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <car-filter></car-filter>
+  </div>
+</template>
+
+<script>
+import CarFilter from './CarFilter.vue';
+
+export default {
+  components: {
+    CarFilter
+  }
+};
+</script>
